f0ca5e064db78b74d60d66686a233f008be2fde0
Takashi Yoshida <takashi@takashiyoshida.org>
2013-10-28 08:25:51 +0800

    Bump SpamSieve version to 2.9.10 (Mavericks compatible)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class spamsieve {
   package { 'spamsieve':
     provider => 'appdmg',
-    source   => 'http://c-command.com/downloads/SpamSieve-2.9.8.dmg'
+    source   => 'http://c-command.com/downloads/SpamSieve-2.9.10.dmg'
   }
 }
--- spec/classes/spamsieve_spec.rb
+++ spec/classes/spamsieve_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-source_url = 'http://c-command.com/downloads/SpamSieve-2.9.8.dmg'
+source_url = 'http://c-command.com/downloads/SpamSieve-2.9.10.dmg'
 describe 'spamsieve' do
   it do
     should contain_class('spamsieve')
